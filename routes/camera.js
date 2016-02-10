5fcab85d5ad5e99958655467e66b740780f8f3de
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-10 18:06:40 +0900

    Use namespace for service separation

--- routes/camera.js
+++ routes/camera.js
@@ -1,11 +1,17 @@
 'use strict';
 
 import {Router} from 'express';
+import socket from '../lib/socket';
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
   res.json({});
+});
+
+router.get('/take', (req, res, next) => {
+  socket.emitTakePicture();
+  res.json({"take": ""});
 });
 
 export default router;
